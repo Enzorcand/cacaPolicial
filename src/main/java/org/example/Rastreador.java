abbdc3f2e6168c8f792fc3054638239e042f9648
Enzorcand <enzorcand@gmail.com>
2024-03-30 15:16:13 -0300

    setup inicial do trabalho

---
+++ src/main/java/org/example/Rastreador.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Rastreador {
+    private char[][] mapa;
+    private int valorTotal;
+
+    public Rastreador(char[][] mapa){
+        this.mapa = mapa;
+        valorTotal = 0;
+    }
+
+    public void iniciaRastreio(){
+        
+    }
+}
