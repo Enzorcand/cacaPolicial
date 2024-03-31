db03596cf4f07e279ef6f67b162a79ef8030428d
Enzorcand <enzorcand@gmail.com>
2024-03-31 15:04:49 -0300

    Leitor de mapa e casos de teste

---
+++ src/main/java/org/example/Leitor.java
@@ -0,0 +1,48 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+public class Leitor {
+    private Path path;
+    public Leitor(int caso){
+        selecionaCaso(caso);
+    }
+
+    private void selecionaCaso(int caso) {
+        switch (caso){
+            case 1 -> path = Paths.get("casosD/casoD50.txt");
+            case 2 -> path = Paths.get("casosD/casoD100.txt");
+            case 3 -> path = Paths.get("casosD/casoD200.txt");
+            case 4 -> path = Paths.get("casosD/casoD500.txt");
+            case 5 -> path = Paths.get("casosD/casoD750.txt");
+            case 6 -> path = Paths.get("casosD/casoD1000.txt");
+            case 7 -> path = Paths.get("casosD/casoD1500.txt");
+            case 8 -> path = Paths.get("casosD/casoD2000.txt");
+        }
+    }
+
+    public char[][] leMapa(){
+        try (BufferedReader reader = Files.newBufferedReader(path)){
+            String line = reader.readLine();
+            Scanner scanner = new Scanner(line).useDelimiter(" ");
+            int i = scanner.nextInt();
+            int j = scanner.nextInt();
+            int nLinha = 0;
+            char[][] mapa = new char[i][j];
+            while ((line = reader.readLine()) != null){
+                char[] array = line.toCharArray();
+                System.arraycopy(array, 0, mapa[nLinha], 0, array.length);
+                nLinha++;
+            }
+            return mapa;
+        } catch (IOException e){
+            System.err.format("Erro de E/S: %s%n", e);
+        }
+        return null;
+    }
+}
